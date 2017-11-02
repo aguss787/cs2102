f70f9fc37908277ce69df5a2268169fa80c109d6
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2017-11-02 14:08:42 +0800

    Add getOffer*

--- controller/offerController.php
+++ controller/offerController.php
@@ -1,8 +1,25 @@
 <?php
     include_once __DIR__ . "/../model/offer.php";
+    include_once __DIR__ . "/../model/acceptedOffer.php";
 
     function getOfferByTaker($email, $limit = NULL) {
         return Offer::findAll("t_email = $email", $limit);
+    }
+
+    function getAcceptedOffersByTaker($t_email, $limit = NULL) {
+        return AcceptedOffer::findAll("t_email = '$t_email'", $limit);
+    }
+
+    function getAcceptedOffersByUser($p_owner, $limit = NULL) {
+        return AcceptedOffer::findAll("p_owner = '$p_owner'", $limit);
+    }
+
+    function getPendingOffersByTaker($t_email, $limit = NULL) {
+        return Offer::findAll("t_email = '$t_email'", $limit);
+    }
+
+    function getPendingOffersByUser($p_owner, $limit = NULL) {
+        return Offer::findAll("p_owner = '$p_owner'", $limit);
     }
 
 ?>
