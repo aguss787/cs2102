655e56655f09551be5c3ecf1424c7b2f7213de1b
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2017-11-02 17:07:50 +0800

    implement rating

--- controller/offerController.php
+++ controller/offerController.php
@@ -1,5 +1,6 @@
 <?php
     include_once __DIR__ . "/../model/offer.php";
+    include_once __DIR__ . "/../model/taker.php";
     include_once __DIR__ . "/../model/acceptedOffer.php";
     include_once __DIR__ . "/../model/querySet.php";
 
@@ -34,16 +35,48 @@
 
     function acceptOffer($p_name, $p_owner, $t_email) {
         $db = startTransaction();
-        $offer = Offer::loadFromDbWithTransaction($p_name, $p_owner, $t_email);
-        $newOffer = AcceptOffer::withProperties(
-            $offer->p_owner,
-            $offer->p_name,
-            $offer->t_email,
-            $offer->care_start_date,
-            $offer->care_end_date
-        );
-        $newOffer->save($db);
-        deleteOffer($p_name, $p_owner, $t_email, $db);
-        commitTransaction($db);
+        try {
+            $offer = Offer::loadFromDbWithTransaction($p_name, $p_owner, $t_email);
+            $newOffer = AcceptOffer::withProperties(
+                $offer->p_owner,
+                $offer->p_name,
+                $offer->t_email,
+                $offer->care_start_date,
+                $offer->care_end_date
+            );
+            $newOffer->save($db);
+            deleteOffer($p_name, $p_owner, $t_email, $db);
+            commitTransaction($db);
+        } catch (Exception $e) {
+            rollbackTransaction($db);
+        }
+    }
+
+    function rateOffer($p_owner, $p_name, $t_email, $rate) {
+        if($rate == 1) {
+            $rate = 'one';
+        } else if($rate == 2) {
+            $rate = 'two';
+        } else if($rate == 3) {
+            $rate = 'three';
+        } else if($rate == 4) {
+            $rate = 'four';
+        } else if($rate == 5) {
+            $rate = 'five';
+        }
+
+        $rate = $rate . '_star';
+
+        $db = beginTransaction();
+        try {
+            $offer = AcceptedOffer::loadFromDbWithTransaction($p_name, $p_owner, $t_email);
+            $taker = Taker::loadFromDbWithTransaction($t_email, $db);
+            $taker->$rate = $taker->$rate + 1;
+            $taker->save($db);
+            $offer->delete($db);
+            commitTransaction($db);
+        } catch (Exception $e) {
+            rollbackTransaction($db);
+        }
     }
 ?>
