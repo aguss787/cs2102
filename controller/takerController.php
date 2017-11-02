38c016cb877a975c9c1bed4f5cf51fc91f3d9166
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2017-11-02 17:11:22 +0800

    Add rating search

--- controller/takerController.php
+++ controller/takerController.php
@@ -19,12 +19,15 @@
         return count(Taker::findAll("email = $email")) > 0;
     }
 
-    function filterTakers($name, $pref, $num = NULL) {
+    function filterTakers($name, $pref, $rating, $num = NULL) {
+        $sumStar = '(one_star + 2 * two_star + 3 * three_star + 4 * four_star + 5 * five_star)';
+        $totalRating = '(one_star + two_star + three_star + four_star + five_star)';
         $res = Taker::getQuerySet()->from("taker, users")
                                    ->select(Taker::$fieldName);
                                    ->filter('taker.email = user.email')
                                    ->filter("(users.first_name || ' ' || users.last_name) LIKE '%$name%'")
                                    ->filter("taker.preference LIKE '%$pref%'")
+                                   ->filter("($sumStar / $totalRating) >= $rating")
                                    ->limit($num);
 
         return Taker::fromQuerySet($res);
