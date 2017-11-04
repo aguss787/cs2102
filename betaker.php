27a5b76f6c0e98f8986d6a70f28edea88ae4e56c
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2017-11-04 14:59:44 +0800

    Fix taker option in the navbar

--- betaker.php
+++ betaker.php
@@ -1,6 +1,51 @@
 <?php
     session_start();
 
+    $logged_in = isset($_SESSION['email']);
+
+    if(!$logged_in) {
+        header('Location:./index.php');
+    }
+?>
+
+<?php
+  include_once __DIR__ . '/controller/userController.php';
+
+  if (!$logged_in) {
+    echo "Nope belom login";
+    exit();
+  }
+
+  upgradeToTaker($_SESSION['email']);
+  header('Location:./index.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Why are you here?</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+
+<?php include 'navbar.php';?>
+
+<div class="container">
+<div class="center">
+<h1>???</h1>
+Why are you here?
+get the f*ck out.
+</div>
+</div>
+</body>
+</html>
+
+
+<?php
+    session_start();
+
     include_once __DIR__ . '/controller/userController.php';
 
     $loged_in = isset($_SESSION['email']);
