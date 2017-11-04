6b2abef86434f9864297ee7f0dc0e6b206ac8df6
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2017-11-04 10:34:51 +0800

    Add redirection for add pet

--- add_pet.php
+++ add_pet.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-	
+
     $logged_in = isset($_SESSION['email']);
 
     if(!$logged_in) {
@@ -10,14 +10,15 @@
 
 <?php
     include_once __DIR__ . '/controller/petController.php';
-	include_once __DIR__ . '/controller/userController.php';
-	
-	$user = getCurrentUser();
+  include_once __DIR__ . '/controller/userController.php';
 
-	/* THE DEFAULT VALUE OF ADDRESS AND CONTACT ARE THE OWNER'S*/
+  $user = getCurrentUser();
+
+  /* THE DEFAULT VALUE OF ADDRESS AND CONTACT ARE THE OWNER'S*/
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && $logged_in) {
         addPet($_SESSION['email'], $_POST['name'], $_POST['type'], $_POST['description'],
             $user->address, $user->contact_number);
+        header('Location:./index.php');
     }
 ?>
 
@@ -27,13 +28,13 @@
     <title>Add Pet</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="stylesheet.css">
+  <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
 
 <?php include 'navbar.php';?>
 
-<div class="container"> 
+<div class="container">
 <div class="center">
 <h1>Add Pet</h1>
 
